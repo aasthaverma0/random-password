b1e4d4622accae4cccc197f693c87c1260515ba5
aasthaverma0 <68563880+aasthaverma0@users.noreply.github.com>
2020-08-04 00:41:44 -0700

    Add files via upload

--- password.py
+++ password.py
@@ -5,7 +5,16 @@ import string
 def get_random_alphanumeric_string(len):
     letters_digits_symbols = string.ascii_letters + string.digits + string.punctuation
     result_str = ''.join(random.choice(letters_digits_symbols) for i in range(len))
-    print("Random password is", "name" + result_str)
-    return "Random password is name" + result_str
+    travel_list = ['river', 'lake', 'mountain', 'desert', 'waterfall', 'paris', 'india', 'alaska', 'canada', 'sydney', 'egypt']
+    entertainment_list = ['cook', 'bake', 'swim', 'fly', 'movie', 'netflix', 'music', 'theater', 'run', 'walk']
+    food_list = ['pizza', 'candy', 'chocolate', 'taco', 'noodle', 'sugar', 'ramen', 'gummy', 'cookie', 'cake', 'boba']
+    print("Random password is " + random.choice(travel_list) + result_str)
+    return "Random password is " + random.choice(travel_list) + result_str
+    print("Random password is " + random.choice(entertainment_list) + result_str)
+    return "Random password is " + random.choice(entertainment_list) + result_str
+    print("Random password is " + random.choice(food_list) + result_str)
+    return "Random password is " + random.choice(food_list) + result_str
 
+get_random_alphanumeric_string(6)
+get_random_alphanumeric_string(6)
 get_random_alphanumeric_string(6)
